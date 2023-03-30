983738b67b4b9082e399ae44ea9c16825e85d0f4
thaksin5 <114573651+thaksin5@users.noreply.github.com>
2023-03-30 19:19:48 +0800

    upload file

---
+++ creational_pattern/Singleton/newCppSingleton.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// new version is not detect a memory leak and thread safe(c++11
+class GameSetting{
+    private:
+        int _width;
+        int _height;
+        GameSetting():_width(1920), _height(1080){}
+        ~GameSetting(){};
+    public:
+        static GameSetting& getInstance(){
+            static GameSetting instance;
+            return instance;
+        }
+        void setWidth(int width){ _width = width;}
+        void setHeight(int height){ _height = height;}
+        void displayResolution(){
+            char str[80] = {};
+            sprintf(str,"Resolution : %d x %d", _width, _height);
+            cout<<str<<endl;
+        }
+};
+
+int main(){
+    GameSetting &setting1 = GameSetting::getInstance();
+    setting1.displayResolution();
+    GameSetting &setting2 = GameSetting::getInstance();
+    setting2.setWidth(800);
+    setting2.setHeight(700);
+    setting2.displayResolution();
+    
+    return 0;
+}
